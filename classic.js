77823ef5a52711329b4ca7cc5a5be292e8a52807
Neal Granger <neal@metalab.co>
2015-11-03 02:36:54 -0800

    Presets for expected project configurations. Make it easier for consumers on projects to easily discover and pick the rules that apply. Presets are designed to match common use cases.
    Example usage in `.eslintrc`:
    
    ```json
    {
      "extends": [
        "metalab/standard",
        "metalab/node"
      ]
    }
    ```

---
+++ classic.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    './rules/best-practices.js',
+    './rules/docs.js',
+    './rules/errors.js',
+    './rules/filenames.js',
+    './rules/import.js',
+    './rules/style.js',
+  ],
+};
---
+++ standard.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    './rules/best-practices.js',
+    './rules/babel.js',
+    './rules/docs.js',
+    './rules/errors.js',
+    './rules/filenames.js',
+    './rules/import.js',
+    './rules/modern.js',
+    './rules/style.js',
+  ],
+};
