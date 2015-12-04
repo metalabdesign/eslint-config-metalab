d20bc33cb258fce751bfa95017ef3642461c3f07
Neal Granger <neal@metalab.co>
2015-12-03 19:03:26 -0800

    Separate environment presets from syntax presets.
    For any project setup you will need to extend an environment preset and a syntax preset, e.g. `"extends": ["metalab/legacy", "metalab/node"]`.

--- node.js
+++ node.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: [
-    './base.js',
     './rules/node.js',
   ].map(require.resolve),
 };
--- react.js
+++ react.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: [
-    './base.js',
     './rules/react.js',
   ].map(require.resolve),
 };
