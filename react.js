fe3d6f59a60129447c4faa4eaaf6c12b9c675149
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-01-25 22:36:32 -0800

    Split `react` into `react` and `jsx`.
    As part of the split couple rule updates add:
     * Add spacing rule.
     * Add proper JSX variable consumption.

--- react.js
+++ react.js
@@ -1,5 +1,12 @@
 module.exports = {
+  settings: {
+    react: {
+      pragma: 'createElement',
+    },
+  },
   extends: [
+    './base.js',
+    './rules/jsx.js',
     './rules/react.js',
   ].map(require.resolve),
 };
